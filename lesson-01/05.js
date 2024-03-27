5da57907c67dfef49f2c3730356555cd0bd8186f
Anastasia <lovly7@yandex.ru>
2024-03-27 16:35:41 +0300

    05

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,14 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 let y = rule.length;
-switch(y) {
-case y < 25: 
-console.log("Все таки нет правил без исключения");
-break;
-
-case y > 25: 
-console.log("У меня 100 пудов все получится");
-break;
+switch(true) {
+case y < 25: {    
+    console.log("Все таки нет правил без исключения");
+    break;
+}
+case y > 25: {
+    console.log("У меня 100 пудов все получится");
+    break;
+}
 
 default:
 console.log('«50 на 50»');
